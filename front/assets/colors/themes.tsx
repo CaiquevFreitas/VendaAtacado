944d6f9d80ef9049f2870f6cee328de96819ff95
Caique Vidal <142412587+CaiquevFreitas@users.noreply.github.com>
2025-03-26 19:09:29 -0300

    Update themes.tsx

--- front/assets/colors/themes.tsx
+++ front/assets/colors/themes.tsx
@@ -1,7 +1,7 @@
 export const themes = {
     colors:{
-        primary: "",
-        secondary: "",
-        black: ""
+        primary: "#295f98",
+        secondary: "#c7c7cc",
+        black: "#1e1e1e"
     }
 }
