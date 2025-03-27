23967875c3392f4fca789269177e9c5b2ad4628c
CaiquevFreitas <caique.v.freitas@ba.estudante.senai.br>
2025-03-27 10:34:42 -0300

    Create navegação do cliente

--- front/app/screens/cliente/home.tsx
+++ front/app/screens/cliente/home.tsx
@@ -1,6 +1,6 @@
 import { View, Text, StyleSheet } from "react-native";
 
-export default function Buscar(){
+export default function Home(){
     return (
         <View>
         <Text>Inicializando</Text>
---
+++ front/routes/clientNavigator.tsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import { createBottomTabNavigator } from  '@react-navigation/bottom-tabs'
+import { NavigationContainer } from '@react-navigation/native'
+import { Ionicons } from '@expo/vector-icons'
+
+import Perfil from '@/app/screens/cliente/perfil'
+import Carrinho from '@/app/screens/cliente/carrinho'
+import Buscar from '@/app/screens/cliente/buscar'
+import Home from '@/app/screens/cliente/home'
+
+const Tab = createBottomTabNavigator();
+
+export default function ClientNavigator(){
+  return (
+    <Tab.Navigator
+  screenOptions={({ route }) => ({
+    tabBarIcon: ({ color, size }) => {
+      let iconName: keyof typeof Ionicons.glyphMap; 
+
+      switch (route.name) {
+        case 'Início':
+          iconName = 'home-outline';
+          break;
+        case 'Busca':
+          iconName = 'search-outline';
+          break;
+        case 'Carrinho':
+          iconName = 'cart-outline';
+          break;
+        case 'Perfil':
+          iconName = 'person-outline';
+          break;
+        default:
+          iconName = 'help-circle-outline'; 
+      }
+      return <Ionicons name={iconName} size={size} color={color} />;
+    },
+  })}>;
+        <Tab.Screen name='Home' component={Home} />
+        <Tab.Screen name="Perfil" component={Perfil} />
+        <Tab.Screen name='Buscar' component={Buscar} />
+        <Tab.Screen name='Carrinho' component={Carrinho} />
+    </Tab.Navigator>
+  )
+}
