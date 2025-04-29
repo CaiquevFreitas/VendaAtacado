5cb64a130bdcb989c442b9a14224570ae4976652
Caique Vidal <142412587+CaiquevFreitas@users.noreply.github.com>
2025-04-29 19:34:55 -0300

    Update cliente.js

--- back/src/models/cliente.js
+++ back/src/models/cliente.js
@@ -4,13 +4,42 @@ const sequelize = require('./database');
 class Cliente extends Model {}
 
 Cliente.init({
-    idCliente: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
-    nomeCliente: { type: DataTypes.STRING(255), allowNull: false },
-    email: { type: DataTypes.STRING(100), allowNull: false },
-    cpf: { type: DataTypes.CHAR(11), allowNull: false, unique: true },
-    telefone: { type: DataTypes.CHAR(11), allowNull: false, unique: true },
-    dataNascimento: { type: DataTypes.DATE, allowNull: false },
-    senha: { type: DataTypes.CHAR(8), allowNull: false }
-}, { sequelize, modelName: 'Cliente' });
+    idCliente: { 
+        type: DataTypes.INTEGER, 
+        autoIncrement: true, 
+        primaryKey: true 
+    },
+    nomeCliente: { 
+        type: DataTypes.STRING(255), 
+        allowNull: false 
+    },
+    email: { 
+        type: DataTypes.STRING(100), 
+        allowNull: false 
+    },
+    cpf: { 
+        type: DataTypes.CHAR(11), 
+        allowNull: false, 
+        unique: true 
+    },
+    telefone: { 
+        type: DataTypes.CHAR(11), 
+        allowNull: false, 
+        unique: true 
+    },
+    dataNascimento: { 
+        type: DataTypes.DATE, 
+        allowNull: false 
+    },
+    senha: {
+        type: DataTypes.CHAR(8),
+        allowNull: false
+    }
+}, { 
+    sequelize, 
+    modelName: 'Cliente',
+    tableName: 'cliente', 
+    timestamps: false      
+});
 
-module.exports = Cliente ;
+module.exports = Cliente;
