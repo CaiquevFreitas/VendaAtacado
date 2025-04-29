a1e6152038c3b51d8c6065469dd6f696a0b4e004
Caique Vidal <142412587+CaiquevFreitas@users.noreply.github.com>
2025-04-29 19:40:15 -0300

    Update app.js

--- back/app.js
+++ back/app.js
@@ -3,22 +3,24 @@ const port = 3000;
 const cors = require('cors');
 const dotenv = require('dotenv');
 const db = require('./src/models/database');
-
 dotenv.config();
-
 const app = express();
 app.use(cors({
-    origin: 'exp://192.168.1.7:8081',
+    origin: 'exp://localhost:8081',
     methods: ['GET', 'POST', 'DELETE', 'PUT'],
     allowedHeaders: ['Content-Type']
 }))
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
 
+//Importação das Rotas
+const cadastroclienteRoute = require('./src/routes/cadastroCliente')
+
 app.get('/', (req, res) => {
     res.send('API funcionando!');
 });
 
+app.use('/', cadastroclienteRoute);
 
 app.listen(3000, () => {
     console.log(`Servidor rodando: http://localhost:${port}`);
