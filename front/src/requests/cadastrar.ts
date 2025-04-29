b6365f4c69e3ed38b54411224c04a9dd37dc6e06
Caique Vidal <142412587+CaiquevFreitas@users.noreply.github.com>
2025-04-29 19:34:20 -0300

    Update cadastrar.ts

--- front/src/requests/cadastrar.ts
+++ front/src/requests/cadastrar.ts
@@ -3,7 +3,7 @@ import { Alert } from "react-native";
 export async function cadastrar(nome:string, data:Date, cpf:string, email:string, senha:string, telefone:string){
   const dataFormatada = data ? data.toISOString().split('T')[0] : '';
     try {
-        const response = await fetch('http://localhost:3000/cadastroCliente', {
+        const response = await fetch('http://192.168.121.96:3000/cadastroCliente', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
@@ -21,10 +21,10 @@ export async function cadastrar(nome:string, data:Date, cpf:string, email:string
         const dados = await response.json();
   
         if (response.ok) {
-          Alert.alert('Sucesso', 'Login realizado!');
+          Alert.alert('Sucesso', 'Cadastro realizado!');
           console.log('Token ou dados:', data);
         } else {
-          Alert.alert('Erro', dados.message || 'Falha no login');
+          Alert.alert('Erro', dados.message || 'Falha no Cadastro');
         }
       } catch (error) {
         console.error('Erro:', error);
