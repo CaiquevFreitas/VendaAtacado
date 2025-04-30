3ad1e815656c98f229274086e757f6270ee9c26c
Caique Vidal <142412587+CaiquevFreitas@users.noreply.github.com>
2025-04-30 19:55:13 -0300

    Update loginCliente.js

--- back/src/routes/loginCliente.js
+++ back/src/routes/loginCliente.js
@@ -2,12 +2,25 @@ const express = require('express');
 const Cliente = require('../models/cliente');
 const router = express.Router();
 
-router.get('/loginCliente', (req,res) =>{
-    const [email, senha] = req.body
+router.post('/loginCliente', async(req,res) =>{
+    const {email, senha} = req.body
+    console.log(`Email: ${email}, senha:${senha}`);
 
-    try {
-        
-    } catch (error) {
-        
+    const user = await Cliente.findOne({
+        where: {email: email}
+    })
+    
+    if (user == null){
+            res.status(200).json({ status: 0, message: 'usuário não encontrado.' });
+            return;
     }
+
+    if(user.senha != senha){
+        res.status(200).json({ status: 0, message: 'senha incorreta.'});
+        return;
+    }
+
+    res.status(200).json({message:'Tudo certo'})
 })
+
+module.exports = router;
