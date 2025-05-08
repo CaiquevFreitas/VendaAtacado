916c1154399d29c56939195d5466bcd9d4b32387
Mvlm-lp <mvinnylopes01@gmail.com>
2025-05-07 21:07:10 -0300

    Create logarLoja.ts

---
+++ front/src/requests/logarLoja.ts
@@ -0,0 +1,29 @@
+import { Alert } from "react-native";
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+export async function logarLoja(email: string, senha: string): Promise<boolean> {
+  try {
+    const response = await fetch('http://192.168.69.96:3000/loginLoja', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ email, senha }),
+    });
+
+    const dados = await response.json();
+
+    if (response.ok) {
+      await AsyncStorage.setItem('userType', 'loja');
+      Alert.alert('Sucesso', 'Login da loja realizado!');
+      return true;
+    } else {
+      Alert.alert('Erro', dados.message || 'Falha no Login');
+      return false;
+    }
+  } catch (error) {
+    console.error('Erro:', error);
+    Alert.alert('Erro', 'Não foi possível conectar ao servidor.');
+    return false;
+  }
+}
