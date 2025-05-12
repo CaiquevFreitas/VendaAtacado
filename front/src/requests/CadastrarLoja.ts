d2b0e41fc4d15f2b805ecf1d36b275f363154401
Mvlm-lp <mvinnylopes01@gmail.com>
2025-05-12 20:51:32 -0300

    Update CadastrarLoja.ts

--- front/src/requests/CadastrarLoja.ts
+++ front/src/requests/CadastrarLoja.ts
@@ -2,13 +2,21 @@ import { Alert } from "react-native";
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export async function cadastrarLoja(
+  nomeVendedor: string,
   nomeLoja: string,
   cpf: string,
-  endereco: string,
-  horario: string,
   telefone: string,
   email: string,
-  senha: string
+  senha: string,
+  dataNascimento: string,
+  confirmarSenha: string,
+  horario: string,
+  logo: string,
+  cep: string,          // Adicionando o parâmetro cep
+  rua: string,          // Adicionando o parâmetro rua
+  bairro: string,       // Adicionando o parâmetro bairro
+  cidade: string,       // Adicionando o parâmetro cidade
+  estado: string        // Adicionando o parâmetro estado
 ): Promise<boolean> {
   try {
     const response = await fetch('http://192.168.69.96:3000/cadastroLoja', {
@@ -17,13 +25,21 @@ export async function cadastrarLoja(
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({
+        nomeVendedor,
         nomeLoja,
         cpf,
-        endereco,
-        horario,
         telefone,
         email,
-        senha
+        senha,
+        dataNascimento,
+        confirmarSenha,
+        horario,
+        logo,
+        cep,           // Enviando o parâmetro cep
+        rua,           // Enviando o parâmetro rua
+        bairro,        // Enviando o parâmetro bairro
+        cidade,        // Enviando o parâmetro cidade
+        estado         // Enviando o parâmetro estado
       }),
     });
 
