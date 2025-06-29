b96ec84aa8e8e4c8083aeea0c0e2fcbdeed894ce
CaiquevFreitas <caique.v.freitas@ba.estudante.senai.br>
2025-06-29 09:55:39 -0300

    Model Notificação

---
+++ back/src/models/notificacao.js
@@ -0,0 +1,17 @@
+const { DataTypes, Model } = require('sequelize');
+const sequelize = require('./database');
+const Cliente = require('./cliente');
+const Loja = require('./loja');
+
+class Notificacao extends Model {}
+
+Notificacao.init({
+    idNotificacao: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
+    titulo: { type: DataTypes.STRING(100), allowNull: false },
+    descricao: { type: DataTypes.STRING(200), allowNull: false },
+    dataNotificacao: { type: DataTypes.DATE, allowNull: false },
+    fk_idCliente: { type: DataTypes.INTEGER, allowNull: true, references: { model: Cliente, key: 'idCliente' } },
+    fk_idLoja: { type: DataTypes.INTEGER, allowNull: true, references: { model: Loja, key: 'idLoja' } },
+}, { sequelize, modelName: 'Notificacao', tableName: 'notificacao', timestamps: false });
+
+module.exports = Notificacao;
